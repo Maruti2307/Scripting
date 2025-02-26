0b707870d9b67c4204606f4669dfe3929b91da78
Maruti2307 <chavanmac23@gmail.com>
2025-02-26 21:12:47 +0530

    new

--- Script1.sh
+++ Script1.sh
@@ -1,3 +1,9 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
+<<<<<<< HEAD
 echo "We are from batch-27 and learning DevOps"
+=======
+echo "We are from batch-27 and learning DevOps"
+echo "I am working in REQ092348"
+echo "i have complete devops course with full"
+>>>>>>> ed8e9a4 (changes)
