97a0ed765ef0a273dffdf465c83564b1dfbe37b0
Maruti2307 <chavanmac23@gmail.com>
2025-03-04 10:02:36 +0530

    devops chages

--- Script1.sh
+++ Script1.sh
@@ -6,4 +6,5 @@ echo "We are from batch-27 and learning DevOps"
 echo "We are from batch-27 and learning DevOps"
 echo "I am working in REQ092348"
 echo "i have complete devops course with full"
+echo "i am learned devops smartally"
 >>>>>>> ed8e9a4 (changes)
